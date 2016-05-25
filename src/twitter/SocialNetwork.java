eb394b0197f5844f37460d3ed282e4e6db05ac29
Alex <alexdev-eclipse@sapo.pt>
2016-05-25 12:56:52 +0100

    Implemented guessFollowsGraph & Testsuit testGuessFollowsGrapth*()

--- src/twitter/SocialNetwork.java
+++ src/twitter/SocialNetwork.java
@@ -1,5 +1,11 @@
 package twitter;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -32,9 +38,31 @@ public class SocialNetwork {
      *         tweets. One kind of evidence that Ernie follows Bert is if Ernie
      *         @-mentions Bert in a tweet. This must be implemented. Other kinds
      *         of evidence may be used at the implementor's discretion.
+     *         
+     *         All the Twitter usernames in the returned social network must be 
+     *         either authors or @-mentions in the list of tweets.
      */
-    public static Map<String, Set<String>> guessFollowsGraph(List<Tweet> tweets) {
-        throw new RuntimeException("not implemented");
+    public static Map<String, Set<String>> guessFollowsGraph(final List<Tweet> tweets) {
+        //throw new RuntimeException("not implemented");
+        Map<String, Set<String>> snet = new HashMap<String, Set<String>>();
+        String member;
+        Set<String> personsToFollow;
+        
+        if(tweets.isEmpty()){
+            return snet;
+        }else{
+            for(Tweet tweet: tweets){
+                member = tweet.getAuthor();
+                personsToFollow = Extract.getMentionedUsers(Arrays.asList(tweet));
+                if(snet.containsKey(member)) snet.get(member).addAll(personsToFollow);
+                else snet.put(member, personsToFollow);
+                
+            }
+            
+            return snet;
+        }
+        
+       
     }
 
     /**
@@ -47,7 +75,70 @@ public class SocialNetwork {
      *         descending order of follower count.
      * 
      */
-    public static List<String> influencers(Map<String, Set<String>> followsGraph) {
-        throw new RuntimeException("not implemented");
+    public static List<String> influencers(final Map<String, Set<String>> followsGraph) {
+        //throw new RuntimeException("not implemented");
+        // I've choosen MY simpliest approach to sort map values by extracting map values to list and using helper class UserFollows.
+        // On 'stackoverflow' was so many complicated ways..!
+       
+        List<UserFollows> mostFollowed = new ArrayList<UserFollows>();
+        List<String> greatestInflunceList = new ArrayList<String>();        
+        Map<String, Integer> countMentions = new HashMap<String, Integer>();
+        SocialNetwork snet = new SocialNetwork(); // instanceof SocialNetwork needed for using inner Class
+                       
+        if(followsGraph.isEmpty()){
+            return greatestInflunceList;
+        }else{       
+            for(String member: followsGraph.keySet()){ // iterate thru usernames in network
+                for(String personToFollow :followsGraph.get(member)){ // iterate thru set of followers witch user follows                   
+                    if(countMentions.containsKey(personToFollow)){                       
+                        countMentions.put(personToFollow, countMentions.get(personToFollow) + 1); // create map witch map users to max time they mentioned in tweets
+                    }
+                    else
+                        countMentions.put(personToFollow, 1);
+                }
+                
+            }
+            
+            // create mostFollowed list
+            for(String member: countMentions.keySet())
+                mostFollowed.add(snet.new UserFollows(member, countMentions.get(member)));
+            
+            // Sort and Reverse order for most Followed
+            Collections.sort(mostFollowed, snet.new UserComparator());
+            Collections.reverse(mostFollowed); 
+            
+            for(UserFollows member: mostFollowed) System.out.println(member.username + "-->" + member.numFollowers);
+            for(UserFollows member: mostFollowed) greatestInflunceList.add(member.username.substring(1));
+            
+            return greatestInflunceList;
+        }
+    }
+    
+    
+    /**
+     * UserFollows Helper Class will be used in influencers() for comparing users in a list.
+     * @author MyCode
+     *
+     */
+    private class UserFollows{
+        private String username;
+        private int numFollowers;
+        
+        public UserFollows(String username, int followers){
+            this.username = username;
+            numFollowers = followers;
+        }        
+    }
+    
+    private class UserComparator implements Comparator {       
+        @Override
+        public int compare(Object o1, Object o2) {
+            UserFollows u1 = (UserFollows)o1;
+            UserFollows u2 = (UserFollows)o2;
+            if(u1.numFollowers == u2.numFollowers) return 0;
+            else if(u1.numFollowers > u2.numFollowers) return 1;
+            else return -1;
+        }
     }
 }
+
---
+++ test/twitter/SocialNetworkTest.java
@@ -0,0 +1,260 @@
+package twitter;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/*
+ * TODO: your testing strategies for these methods should go here
+ */
+
+public class SocialNetworkTest {
+    private static Date d1;
+    private static Date d2;
+    
+    private static Tweet tweet1;
+    private static Tweet tweet2;
+    
+    private static List<Tweet> tlist;
+
+    @BeforeClass
+    public static void setUpBeforeClass() {
+        Calendar calendar = Calendar.getInstance();
+        
+        calendar.set(2014, 1, 14, 10, 00, 00);
+        d1 = calendar.getTime();
+        
+        calendar.set(2014, 1, 14, 11, 00, 00);
+        d2 = calendar.getTime();
+        
+        tweet1 = new Tweet(0, "alyssa", "is it reasonable to talk about rivest so much?", d1);
+        tweet2 = new Tweet(1, "bbitdiddle", "so rivest talk in 30 minutes #hype", d2);
+        
+        // MyNote: Date and Calendar Classes generate diff timestamps for equal time!
+        tlist = new ArrayList<Tweet>();
+        tlist.add(new Tweet(1, "alyssa", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 10, 00, 00)));
+        tlist.add(new Tweet(2, "max", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 10, 00, 8)));
+        tlist.add(new Tweet(3, "denis", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 10, 00, 25)));
+        tlist.add(new Tweet(4, "ana", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 10, 00, 55)));
+        
+        tlist.add(new Tweet(5, "alyssa", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 11, 00, 2)));
+        tlist.add(new Tweet(6, "denis", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 11, 00, 18)));
+        tlist.add(new Tweet(7, "max", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 11, 00, 23)));
+        tlist.add(new Tweet(8, "ana", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 11, 00, 50)));
+        
+        tlist.add(new Tweet(9, "max", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 12, 00, 01)));
+        tlist.add(new Tweet(10, "alyssa", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 12, 00, 3)));
+        tlist.add(new Tweet(11, "max", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 12, 00, 14)));
+        tlist.add(new Tweet(12, "denis", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 12, 00, 35))); 
+    }
+    
+    @Test
+    public void testGuessFollowsGraphEmpty() { // --> {[] -->[]}
+        Map<String, Set<String>> followsGraph = SocialNetwork.guessFollowsGraph(new ArrayList<Tweet>());
+        
+        assertTrue(followsGraph.isEmpty());
+    }   
+   
+    @Test
+    public void testGuessFollowsGraphMemberFollowsNobody() { // --> {[1,] -->[]}
+        List<Tweet> tlist2 = new ArrayList<Tweet>();
+        tlist2.add(new Tweet(1, "alyssa", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 10, 00, 00)));
+        tlist2.add(new Tweet(2, "max", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 10, 00, 8)));
+        tlist2.add(new Tweet(3, "denis", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 10, 00, 25)));
+        tlist2.add(new Tweet(4, "ana", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 10, 00, 55)));
+        
+        tlist2.add(new Tweet(5, "alyssa", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 11, 00, 2)));
+        tlist2.add(new Tweet(6, "denis", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 11, 00, 18)));
+        tlist2.add(new Tweet(7, "max", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 11, 00, 23)));
+        tlist2.add(new Tweet(8, "ana", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 11, 00, 50)));
+        
+        tlist2.add(new Tweet(9, "max", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 12, 00, 01)));
+        tlist2.add(new Tweet(10, "alyssa", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 12, 00, 3)));
+        tlist2.add(new Tweet(11, "max", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 12, 00, 14)));
+        tlist2.add(new Tweet(12, "denis", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 12, 00, 35))); 
+        Map<String, Set<String>> followsGraph = SocialNetwork.guessFollowsGraph(tlist2);
+        
+        assertEquals(4, followsGraph.size());
+        assertTrue(followsGraph.get("alyssa").isEmpty());
+        assertTrue(followsGraph.get("denis").isEmpty());
+        assertTrue(followsGraph.get("max").isEmpty());
+        assertTrue(followsGraph.get("ana").isEmpty());
+    }
+    
+    @Test
+    public void testGuessFollowsGraphMemberFollowsOnePerson() { // --> {[1,] -->[1,]} ,others keys mapped to [] set
+        tlist = new ArrayList<Tweet>();
+        tlist.add(new Tweet(1, "alyssa", "is it reasonable to @denis about rivest so much?", new Date(2014, 1, 14, 10, 00, 00)));
+        tlist.add(new Tweet(2, "max", "is it reasonable to talk @alyssa about rivest so much?", new Date(2014, 1, 14, 10, 00, 8)));
+        tlist.add(new Tweet(3, "denis", "is it reasonable to talk @max about rivest so much?", new Date(2014, 1, 14, 10, 00, 25)));
+        tlist.add(new Tweet(4, "ana", "is it reasonable to talk about @denis rivest so much?", new Date(2014, 1, 14, 10, 00, 55)));
+        
+        tlist.add(new Tweet(5, "alyssa", "is it reasonable to talk @denis about rivest so much?", new Date(2014, 1, 14, 11, 00, 2)));
+        tlist.add(new Tweet(6, "denis", "is it reasonable to talk about @max rivest so much?", new Date(2014, 1, 14, 11, 00, 18)));
+        tlist.add(new Tweet(7, "max", "is it reasonable to talk about rivest @alyssa so much?", new Date(2014, 1, 14, 11, 00, 23)));
+        tlist.add(new Tweet(8, "ana", "is it reasonable to talk about rivest so much? @denis", new Date(2014, 1, 14, 11, 00, 50)));
+        
+        tlist.add(new Tweet(9, "max", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 12, 00, 01)));
+        tlist.add(new Tweet(10, "alyssa", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 12, 00, 3)));
+        tlist.add(new Tweet(11, "max", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 12, 00, 14)));
+        tlist.add(new Tweet(12, "denis", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 12, 00, 35))); 
+        
+        Map<String, Set<String>> followsGraph = SocialNetwork.guessFollowsGraph(tlist);
+        
+        assertEquals(4, followsGraph.size());
+        assertEquals(1, followsGraph.get("alyssa").size());
+        assertEquals(1, followsGraph.get("denis").size());
+        assertEquals(1, followsGraph.get("max").size());
+        assertEquals(1, followsGraph.get("ana").size());
+    }
+    
+    @Test
+    public void testGuessFollowsGraphMemberFollowsMultiplePersons() { // --> {[1,...] -->[1,...]} ,others keys mapped to [] set
+        tlist = new ArrayList<Tweet>();
+        tlist.add(new Tweet(1, "alyssa", "is it reasonable to @denis about rivest so much?", new Date(2014, 1, 14, 10, 00, 00)));
+        tlist.add(new Tweet(2, "max", "is it reasonable to talk @allysa about rivest so much?", new Date(2014, 1, 14, 10, 00, 8)));
+        tlist.add(new Tweet(3, "denis", "is it reasonable to talk @max about rivest so much?", new Date(2014, 1, 14, 10, 00, 25)));
+        tlist.add(new Tweet(4, "ana", "is it reasonable to talk about @denis rivest so much?", new Date(2014, 1, 14, 10, 00, 55)));
+        
+        tlist.add(new Tweet(5, "alyssa", "is it reasonable to talk @max about rivest so much?", new Date(2014, 1, 14, 11, 00, 2)));
+        tlist.add(new Tweet(6, "denis", "is it reasonable to talk @ana @alyssa about rivest so much?", new Date(2014, 1, 14, 11, 00, 18)));
+        tlist.add(new Tweet(7, "max", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 11, 00, 23)));
+        tlist.add(new Tweet(8, "ana", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 11, 00, 50)));
+        
+        tlist.add(new Tweet(9, "max", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 12, 00, 01)));
+        tlist.add(new Tweet(10, "alyssa", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 12, 00, 3)));
+        tlist.add(new Tweet(11, "max", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 12, 00, 14)));
+        tlist.add(new Tweet(12, "denis", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 12, 00, 35))); 
+        
+        Map<String, Set<String>> followsGraph = SocialNetwork.guessFollowsGraph(tlist);
+        
+        assertEquals(4, followsGraph.size());
+        assertEquals(2, followsGraph.get("alyssa").size());
+        assertEquals(3, followsGraph.get("denis").size());
+        assertEquals(1, followsGraph.get("max").size());
+        assertEquals(1, followsGraph.get("ana").size());
+    }
+    
+    @Test
+    public void testGuessFollowsGraphUniqueUsernameCheck() {
+        // this test is not necessary as in this case Map keys is mapped to Sets, so if found key the new
+        // value will union to old key values. No to equal keys with diff values are possible by code design.
+    }
+    
+    
+    
+    /* 2nd test suit------------------------------------------------------------------------------------------*/
+    @Test
+    public void testInfluencersEmptySocialNetwork() {
+        Map<String, Set<String>> followsGraph = SocialNetwork.guessFollowsGraph(new ArrayList<Tweet>());
+        List<String> influencers = SocialNetwork.influencers(followsGraph);
+        
+        assertTrue(influencers.isEmpty());
+        
+    }
+    
+    @Test
+    public void testInfluencersActiveNetworkAndNoFollowers() { // --> []
+        List<Tweet> tlist2 = new ArrayList<Tweet>();
+        tlist2.add(new Tweet(1, "alyssa", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 10, 00, 00)));
+        tlist2.add(new Tweet(2, "max", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 10, 00, 8)));
+        tlist2.add(new Tweet(3, "denis", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 10, 00, 25)));
+        tlist2.add(new Tweet(4, "ana", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 10, 00, 55)));
+        
+        tlist2.add(new Tweet(5, "alyssa", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 11, 00, 2)));
+        tlist2.add(new Tweet(6, "denis", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 11, 00, 18)));
+        tlist2.add(new Tweet(7, "max", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 11, 00, 23)));
+        tlist2.add(new Tweet(8, "ana", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 11, 00, 50)));
+        
+        tlist2.add(new Tweet(9, "max", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 12, 00, 01)));
+        tlist2.add(new Tweet(10, "alyssa", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 12, 00, 3)));
+        tlist2.add(new Tweet(11, "max", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 12, 00, 14)));
+        tlist2.add(new Tweet(12, "denis", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 12, 00, 35))); 
+        Map<String, Set<String>> followsGraph = SocialNetwork.guessFollowsGraph(tlist2);        
+        List<String> influencers = SocialNetwork.influencers(followsGraph);
+        System.out.println("influncers size test: "+influencers.size());
+        assertTrue(influencers.isEmpty());
+        assertEquals(0, followsGraph.get("alyssa").size());
+        assertEquals(0, followsGraph.get("denis").size());
+        assertEquals(0, followsGraph.get("max").size());
+        assertEquals(0, followsGraph.get("ana").size());
+    }
+    
+    @Test
+    public void testInfluencersDesendingOrderCheck() {
+        tlist = new ArrayList<Tweet>();
+        tlist.add(new Tweet(1, "alyssa", "is it reasonable to     @denis about rivest so much?", new Date(2014, 1, 14, 10, 00, 00)));
+        tlist.add(new Tweet(2, "max", "is it reasonable to talk   @alyssa about rivest so much?", new Date(2014, 1, 14, 10, 00, 8)));
+        tlist.add(new Tweet(3, "denis", "is it reasonable to talk @max about rivest so much?", new Date(2014, 1, 14, 10, 00, 25)));
+        tlist.add(new Tweet(4, "ana", "is it reasonable to talk a @denis rivest so much?", new Date(2014, 1, 14, 10, 00, 55)));
+        
+        tlist.add(new Tweet(5, "alyssa", "is it reasonable to tal @max about rivest so much?", new Date(2014, 1, 14, 11, 00, 2)));
+        tlist.add(new Tweet(6, "denis", "is it reasonable to talk @ana @alyssa about rivest so much?", new Date(2014, 1, 14, 11, 00, 18)));
+        tlist.add(new Tweet(7, "max", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 11, 00, 23)));
+        tlist.add(new Tweet(8, "ana", "is it reasonable to talk a @max rivest so much?", new Date(2014, 1, 14, 11, 00, 50)));
+        
+        tlist.add(new Tweet(9, "max", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 12, 00, 01)));
+        tlist.add(new Tweet(10, "alyssa", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 12, 00, 3)));
+        tlist.add(new Tweet(11, "max", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 12, 00, 14)));
+        tlist.add(new Tweet(12, "denis", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 12, 00, 35))); 
+        
+        Map<String, Set<String>> followsGraph = SocialNetwork.guessFollowsGraph(tlist);        
+        List<String> influencers = SocialNetwork.influencers(followsGraph);
+        
+        // Members follow others counter
+        assertEquals(4, followsGraph.size());
+        assertEquals(2, followsGraph.get("alyssa").size());
+        assertEquals(3, followsGraph.get("denis").size());
+        assertEquals(1, followsGraph.get("max").size());
+        assertEquals(2, followsGraph.get("ana").size());
+        
+        // Members influence list in descending order
+        assertEquals("max",influencers.get(0));
+        assertEquals("alyssa",influencers.get(1));
+        assertEquals("denis",influencers.get(2));
+        assertEquals("ana",influencers.get(3));
+    }
+    
+    @Test
+    public void testInfluencersDistinctUsernamesCheck() {
+        // by code design is impossible to have two or more equal usernames in outcome list
+        // but here is checking for 'some' case
+        tlist = new ArrayList<Tweet>();
+        tlist.add(new Tweet(1, "alyssa", "is it reasonable to     @denis about rivest so much?", new Date(2014, 1, 14, 10, 00, 00)));
+        tlist.add(new Tweet(2, "max", "is it reasonable to talk   @alyssa about rivest so much?", new Date(2014, 1, 14, 10, 00, 8)));
+        tlist.add(new Tweet(3, "denis", "is it reasonable to talk @max about rivest so much?", new Date(2014, 1, 14, 10, 00, 25)));
+        tlist.add(new Tweet(4, "ana", "is it reasonable to talk a @denis rivest so much?", new Date(2014, 1, 14, 10, 00, 55)));
+        
+        tlist.add(new Tweet(5, "alyssa", "is it reasonable to tal @max about rivest so much?", new Date(2014, 1, 14, 11, 00, 2)));
+        tlist.add(new Tweet(6, "denis", "is it reasonable to talk @ana @alyssa about rivest so much?", new Date(2014, 1, 14, 11, 00, 18)));
+        tlist.add(new Tweet(7, "max", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 11, 00, 23)));
+        tlist.add(new Tweet(8, "ana", "is it reasonable to talk a @max rivest so much?", new Date(2014, 1, 14, 11, 00, 50)));
+        
+        tlist.add(new Tweet(9, "max", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 12, 00, 01)));
+        tlist.add(new Tweet(10, "alyssa", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 12, 00, 3)));
+        tlist.add(new Tweet(11, "max", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 12, 00, 14)));
+        tlist.add(new Tweet(12, "denis", "is it reasonable to talk about rivest so much?", new Date(2014, 1, 14, 12, 00, 35))); 
+        
+        Map<String, Set<String>> followsGraph = SocialNetwork.guessFollowsGraph(tlist);        
+        List<String> influenceList = SocialNetwork.influencers(followsGraph);
+        int counter = 0;
+        for(String member: influenceList){
+            System.out.println(member+"---------");
+            for(String member2: influenceList){
+                System.out.println(member2+"--");
+                if(member.equals(member2)) counter++;                
+            }
+            assertEquals(1, counter);
+            counter = 0;
+        }
+    }
+
+}
